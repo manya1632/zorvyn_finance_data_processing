e390da2f2653a176e12363f23703d7c782f90f65
manya1632 <manyacs1303@gmail.com>
2026-04-03 15:52:48 +0530

    chore: set up configuration files

---
+++ jest.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  projects: [
+    {
+      displayName: 'unit',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['**/__tests__/*.unit.test.ts'],
+    },
+    {
+      displayName: 'integration',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['**/__tests__/*.integration.test.ts'],
+      globalSetup: './tests/globalSetup.ts',
+      globalTeardown: './tests/globalTeardown.ts',
+    },
+    {
+      displayName: 'property',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['**/__tests__/*.property.test.ts'],
+    },
+  ],
+  collectCoverageFrom: [
+    'src/modules/**/*.service.ts',
+    'src/modules/**/*.controller.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+      functions: 80,
+      branches: 80,
+    },
+  },
+  coverageReporters: ['text', 'lcov'],
+};
+
+export default config;
